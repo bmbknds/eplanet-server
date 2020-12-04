62014ac3f80eb633aadc4a9e88f718484feb1e1c
Trungnt <naisha@Naishas-MacBook-Pro.local>
2020-12-04 17:21:00 +0700

    update

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -1,6 +1,7 @@
 import { sign } from "../../services/jwt";
 import { success } from "../../services/response/";
 import * as menu_en from "./menu_en";
+import * as menu_vn from "./menu_vn";
 export const login = ({ user }, res, next) => {
   return sign(user)
     .then((token) => ({ token, user: user.view(true) }))
@@ -9,13 +10,13 @@ export const login = ({ user }, res, next) => {
 };
 
 export const getMenu = ({ user, body }, res, next) => {
+  const menu = body.language === "vi" ? menu_vn : menu_en;
   if (user.role === "super-admin" || user.role === "admin") {
-    const menu = menu_en.adminMenu;
-    return success(res, 200)(menu);
+    return success(res, 200)(menu.adminMenu);
   }
   if (user.role === "student") {
     const menu = menu_en.studentMenu;
-    return success(res, 200)(menu);
+    return success(res, 200)(menu.studentMenu);
   }
 
   return success(res, 200)([]);
---
+++ src/api/auth/menu_vn.js
@@ -0,0 +1,131 @@
+const teacherMenu = [];
+const studentMenu = [
+  {
+    path: "/dashboard/class-info",
+    name: "Theo dõi khoá học",
+    icon: "fas fa-chalkboard-teacher",
+    exact: true,
+    routes: [],
+  },
+  {
+    path: "/dashboard/account",
+    name: "Thông tin chung",
+    icon: "far fa-user-circle",
+    exact: true,
+    routes: [],
+  },
+  {
+    path: "/dashboard/feedback-history",
+    name: "Lịch sử phản hồi",
+    icon: "far fa-comments",
+    exact: true,
+    routes: [],
+  },
+];
+const adminMenu = [
+  {
+    path: "/dashboard/overview",
+    name: "Dashboard",
+    icon: "fas fa-chalkboard",
+    exact: true,
+    routes: [],
+  },
+  {
+    path: "/dashboard/cours",
+    name: "Khoá học",
+    icon: "fab fa-leanpub",
+    exact: true,
+    routes: [
+      {
+        path: "/dashboard/cours/edit",
+        name: "Chi tiết khoá học",
+        icon: "fab fa-leanpub",
+        exact: true,
+        routes: [],
+        hideInMenu: true,
+      },
+      {
+        path: "/dashboard/cours/add",
+        name: "Thêm khoá học",
+        icon: "fab fa-leanpub",
+        exact: true,
+        routes: [],
+        hideInMenu: true,
+      },
+    ],
+  },
+  {
+    path: "/dashboard/teacher",
+    name: "Giáo viên",
+    icon: "fas fa-chalkboard-teacher",
+    exact: true,
+    routes: [
+      {
+        path: "/dashboard/teacher/edit",
+        name: "Chi tiết giáo viên",
+        icon: "fas fa-chalkboard-teacher",
+        exact: true,
+        routes: [],
+        hideInMenu: true,
+      },
+      {
+        path: "/dashboard/teacher/add",
+        name: "Thêm giáo viên",
+        icon: "fas fa-chalkboard-teacher",
+        exact: true,
+        routes: [],
+        hideInMenu: true,
+      },
+    ],
+  },
+  {
+    path: "/dashboard/student",
+    name: "Học sinh",
+    icon: "fas fa-user-graduate",
+    exact: true,
+    routes: [
+      {
+        path: "/dashboard/student/edit",
+        name: "Chi tiết học sinh",
+        icon: "fas fa-user-graduate",
+        exact: true,
+        routes: [],
+        hideInMenu: true,
+      },
+      {
+        path: "/dashboard/student/add",
+        name: "Thêm học sinh",
+        icon: "fas fa-user-graduate",
+        exact: true,
+        routes: [],
+        hideInMenu: true,
+      },
+    ],
+  },
+  {
+    path: "/dashboard/finance",
+    name: "Tài chính",
+    icon: "fas fa-donate",
+    exact: true,
+    routes: [
+      //   {
+      //     path: '/dashboard/cours/edit',
+      //     name: 'Cours detail',
+      //     icon: <ReadOutlined />,
+      //     exact: true,
+      //     routes: [],
+      //     hideInMenu: true,
+      //   },
+      //   {
+      //     path: '/dashboard/cours/add',
+      //     name: 'Add cours',
+      //     icon: <ReadOutlined />,
+      //     exact: true,
+      //     routes: [],
+      //     hideInMenu: true,
+      //   },
+    ],
+  },
+];
+
+export { teacherMenu, studentMenu, adminMenu };
