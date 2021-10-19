33f8c0b5b5762b3d1118a6759b7d451f63f8cb18
Trung Nguyen <naisha@Naishas-MacBook.local>
2021-10-19 14:28:44 +0700

    fix bug import

--- src/api/student/controller.js
+++ src/api/student/controller.js
@@ -38,12 +38,14 @@ export const destroy = ({ params }, res, next) =>
     .then(success(res, 204))
     .catch(next);
 
-const checkParentAccountExist = (email) => {
-  return User.findOne({ email });
+const checkParentAccountExist = async (email) => {
+  const user = await User.findOne({ email });
+  console.log(email, "-----", user);
+  return user;
 };
 
-const createChild = function (parentId, child) {
-  return Student.create(child).then((childData) => {
+const createChild = async (parentId, child) => {
+  return await Student.create(child).then((childData) => {
     return User.findByIdAndUpdate(
       parentId,
       { $push: { students: childData._id } },
@@ -52,43 +54,48 @@ const createChild = function (parentId, child) {
   });
 };
 
-export const importData = ({ body }, res, next) => {
-  const _data = body.data;
-  (_data || []).forEach(async (item) => {
-    if (item.parentTel && item.parentEmail) {
-      //check parent existed
-      const parent = await checkParentAccountExist(item.parentEmail);
-      let parentId = null;
-      if (parent) {
-        parentId = parent._id;
-      } else {
-        //tạo phụ huynh
-        const parentCreated = await User.create({
-          role: "student",
-          status: "active",
-          name: item.parentName,
-          email: item.parentEmail,
-          password: "123456",
-          phoneNumber: item.parentTel,
-          gender: item.parentGender,
-        });
-        parentId = parentCreated._id;
-      }
+export const importStudent = async (item) => {
+  if (item.parentTel && item.parentEmail) {
+    //check parent existed
 
-      //tạo con
-      await createChild(parentId, {
-        parentId: parentId,
-        studentName: item.studentName,
-        age: item.age,
-        idNumber: item.idNumber,
-        nickName: item.nickName,
-        skype: item.skype,
-        facebook: item.facebook,
-        note: item.note,
+    let parent = await checkParentAccountExist(item.parentEmail);
+    console.log("email", item.parentEmail, parent);
+    let parentId = null;
+    if (parent) {
+      parentId = parent._id;
+    } else {
+      //tạo phụ huynh
+      const parentCreated = await User.create({
+        role: "student",
+        status: "active",
+        name: item.parentName,
+        email: item.parentEmail,
+        password: "123456",
+        phoneNumber: item.parentTel,
+        gender: item.parentGender,
       });
+      parentId = parentCreated._id;
     }
-  });
+    //tạo con
+    await createChild(parentId, {
+      parentId: parentId,
+      studentName: item.studentName,
+      age: item.age,
+      idNumber: item.idNumber,
+      nickName: item.nickName,
+      skype: item.skype,
+      facebook: item.facebook,
+      note: item.note,
+    });
+    return;
+  }
+};
 
+export const importData = async ({ body }, res, next) => {
+  const _data = body.data;
+  for await (const student of _data) {
+    await importStudent(student);
+  }
   return res.status(200).json({
     status: 1,
     message: "Success",
